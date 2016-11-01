9a00f4196e7220bc377991588e85cdafcc5a60f1
mpudota <madhu.c463@gmail.com>
2016-11-01 14:37:59 -0700

    added a new fragment, which is placeholderfragment in main activity. onCreateView call back must be implemented when creating a fragment and method takes three params, inflator and viewgroup, bundle.
    And should return View, so use inflator.inflate method.
    This method takes three params again, which are laylout file and view group and boolean.

--- app/src/main/java/sunshine/android/weather/com/sunshine/MainActivity.java
+++ app/src/main/java/sunshine/android/weather/com/sunshine/MainActivity.java
@@ -1,7 +1,13 @@
 package sunshine.android.weather.com.sunshine;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static class PlaceHolderFragment extends Fragment {
+
+        public PlaceHolderFragment () {
+
+        }
+
+        public View onCreateview(LayoutInflater inflater, ViewGroup group, Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_main, group, false);
+            return rootView;
+        }
+
     }
 }
