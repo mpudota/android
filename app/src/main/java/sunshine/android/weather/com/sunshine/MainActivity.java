4ed0817095ad1a1b08d2f6d77cd391eb16d0a516
mpudota <madhu.c463@gmail.com>
2016-11-11 18:06:52 -0800

    Added an array of fake data, added them to list, added them to adapter and then to ListView. Created listview xml to refer adapter and forecast xml is the one which displays through fragment.

--- app/src/main/java/sunshine/android/weather/com/sunshine/MainActivity.java
+++ app/src/main/java/sunshine/android/weather/com/sunshine/MainActivity.java
@@ -6,7 +6,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new PlaceHolderFragment()).commit();
+        }
     }
 
     public static class PlaceHolderFragment extends Fragment {
@@ -23,11 +32,30 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+
         public View onCreateview(LayoutInflater inflater, ViewGroup group, Bundle savedInstanceState) {
 
+
+
+        String [] forecastArray = {"Today-Sunny-88/53",
+                "Tomorrow-cloud-77/65",
+                "Wed-rainy-69/61",
+                "Thu-Sunny-81/63",
+                "Fri-Snow-85/55",
+                "Sat-Rain-83/57",
+                "Sun-Sunny-85/78"};
+
+
+
+                    List<String> weatherArray = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weatherArray);
+
             View rootView = inflater.inflate(R.layout.fragment_main, group, false);
+
+         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(adapter);
             return rootView;
         }
-
     }
 }
