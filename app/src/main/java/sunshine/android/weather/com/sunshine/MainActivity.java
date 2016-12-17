45b0f9ce6c520cf05cde94458bb316774b08f477
Madhu Pudota <mpudota@mpudota2.mtv.corp.google.com>
2016-12-16 18:48:22 -0800

    added settings activity with temparature settings to choose between celsius and fahrenheit

--- app/src/main/java/sunshine/android/weather/com/sunshine/MainActivity.java
+++ app/src/main/java/sunshine/android/weather/com/sunshine/MainActivity.java
@@ -1,6 +1,7 @@
 package sunshine.android.weather.com.sunshine;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,7 +28,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 
 }
